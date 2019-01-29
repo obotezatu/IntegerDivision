3727eb244ac58b4f4bcb876ac9759a9dc9f230cc
Oleg Botezatu <olbot@inbox.ru>
2019-01-29 17:42:00 +0200

    ultimile corectari

--- src/main/java/com/foxminded/obotezatu/DivisionFormatter.java
+++ src/main/java/com/foxminded/obotezatu/DivisionFormatter.java
@@ -4,15 +4,14 @@ import java.util.Iterator;
 
 public class DivisionFormatter {
 
-	final private String DASH = "--";
-	
 	public String format(DivisionResult divisionResult) {
 		if ((divisionResult.getDividend() < divisionResult.getDivider()) || divisionResult.getDividend() == 0) {
 			return "0";
 		}
 		StringBuilder formattedResult = new StringBuilder();
 		Iterator<Step> stepsIterator = divisionResult.getSteps().iterator();
-		formattedResult.append(formatHead(divisionResult, stepsIterator)).append(formatBody(divisionResult, stepsIterator));
+		formattedResult.append(formatHead(divisionResult, stepsIterator))
+				.append(formatBody(divisionResult, stepsIterator));
 		return formattedResult.toString();
 	}
 
@@ -20,33 +19,43 @@ public class DivisionFormatter {
 		int dividendLength = String.valueOf(divisionResult.getDividend()).length();
 		StringBuilder formattedResult = new StringBuilder();
 		Step currentStep = stepsIterator.next();
-		if (currentStep.getDividerMultiple() == 0) {
+		while (currentStep.getDividerMultiple() == 0) {
 			currentStep = stepsIterator.next();
 		}
 		formattedResult.append(String.format("_%" + dividendLength + "d | %d%n", divisionResult.getDividend(),
 				divisionResult.getDivider()));
 		formattedResult
 				.append(String.format(" %-" + dividendLength + "d |--------%n", currentStep.getDividerMultiple()));
-		formattedResult.append(String.format(" %-" + dividendLength + "s | %d%n", DASH, divisionResult.getResult()));
+		formattedResult.append(String.format(" %-" + dividendLength + "s | %d%n", countDashes(currentStep.getPartialDividend()), divisionResult.getResult()));
 		return formattedResult.toString();
 	}
 
 	private String formatBody(DivisionResult divisionResult, Iterator<Step> stepsIterator) {
 		StringBuilder formattedResult = new StringBuilder();
 		StringBuilder indent = new StringBuilder();
-		Step currentStep = new Step();
+		Step currentStep=null;
 		while (stepsIterator.hasNext()) {
 			currentStep = stepsIterator.next();
-			indent.append(" ");
+			//indent.append(" ");
 			if (currentStep.getPartialDividend() != 0 && currentStep.getDividerMultiple() != 0) {
 				formattedResult.append(String.format("%s_%s%n", indent, currentStep.getPartialDividend()));
 				formattedResult.append(String.format("%s% d%n", indent, currentStep.getDividerMultiple()));
-				formattedResult.append(String.format(" %s%s%n", indent, DASH));
-			} 
+				formattedResult.append(String.format(" %s%s%n", indent, countDashes(currentStep.getPartialDividend())));
+			}
+			indent.append(" ");
 		}
-		indent.append(" ");
+		//indent.append(" ");
 		formattedResult.append(
 				String.format("%s% d", indent, (currentStep.getPartialDividend() - currentStep.getDividerMultiple())));
 		return formattedResult.toString();
+	}
+
+	private String countDashes(long partialDividend) {
+		StringBuilder dashes = new StringBuilder();
+		int dashCount = String.valueOf(partialDividend).length();
+		for (int i = 0; i < dashCount; i++) {
+			dashes.append("-");
+		}
+		return dashes.toString();
 	}
 }
--- src/test/java/com/foxminded/obotezatu/DivisionFormatterTest.java
+++ src/test/java/com/foxminded/obotezatu/DivisionFormatterTest.java
@@ -1,6 +1,7 @@
 package com.foxminded.obotezatu;
 
 import static org.junit.Assert.assertEquals;
+import static java.lang.System.lineSeparator;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,21 +21,20 @@ public class DivisionFormatterTest {
 
 	@Test
 	public void testDivisionFormatterFormat() {
-		expected.append("_78945 | 4").append(System.lineSeparator()).append(" 4     |--------")
-				.append(System.lineSeparator()).append(" --    | 19736").append(System.lineSeparator()).append(" _38")
-				.append(System.lineSeparator()).append("  36").append(System.lineSeparator()).append("  --")
-				.append(System.lineSeparator()).append("  _29").append(System.lineSeparator()).append("   28")
-				.append(System.lineSeparator()).append("   --").append(System.lineSeparator()).append("   _14")
-				.append(System.lineSeparator()).append("    12").append(System.lineSeparator()).append("    --")
-				.append(System.lineSeparator()).append("    _25").append(System.lineSeparator()).append("     24")
-				.append(System.lineSeparator()).append("     --").append(System.lineSeparator()).append("      1");
+		expected.append("_78945 | 4").append(lineSeparator()).append(" 4     |--------").append(lineSeparator())
+				.append(" -     | 19736").append(lineSeparator()).append(" _38").append(lineSeparator()).append("  36")
+				.append(lineSeparator()).append("  --").append(lineSeparator()).append("  _29").append(lineSeparator())
+				.append("   28").append(lineSeparator()).append("   --").append(lineSeparator()).append("   _14")
+				.append(lineSeparator()).append("    12").append(lineSeparator()).append("    --")
+				.append(lineSeparator()).append("    _25").append(lineSeparator()).append("     24")
+				.append(lineSeparator()).append("     --").append(lineSeparator()).append("      1");
 		assertEquals(expected.toString(), divisionFormatter.format(division.divide(78945, 4)));
 	}
 
 	@Test
 	public void testThousandsDivisionFormatterFormat() {
-		expected.append("_10000 | 10").append(System.lineSeparator()).append(" 10    |--------")
-				.append(System.lineSeparator()).append(" --    | 1000").append(System.lineSeparator()).append("     0");
+		expected.append("_10000 | 10").append(lineSeparator()).append(" 10    |--------").append(lineSeparator())
+				.append(" --    | 1000").append(lineSeparator()).append("     0");
 		assertEquals(expected.toString(), divisionFormatter.format(division.divide(10000, 10)));
 	}
 
